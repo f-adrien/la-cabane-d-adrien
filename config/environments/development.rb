e50359190b6821c95b17083f8698282cda09c6f2
Francois Adrien <francois.s.adrien@gmail.com>
2022-07-16 15:16:33 +0200

    working on home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,8 @@ Rails.application.configure do
     config.hosts << "#{subdomain}.lvh.me"
     config.hosts << "www.#{subdomain}.lvh.me"
   end
+
+  config.hosts << "doconnect.eu.ngrok.io"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
