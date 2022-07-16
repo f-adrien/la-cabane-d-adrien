1dc1c6f3b5b03e746febcb3823f058484fb30efd
Francois Adrien <francois.s.adrien@gmail.com>
2022-07-16 16:37:20 +0200

    fixed menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_164130) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_29_164130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -21,7 +20,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -34,7 +33,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
     t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -51,14 +50,14 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
     t.string "last_name"
     t.string "admin_level", default: "seaman"
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -66,16 +65,16 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
   create_table "product_options", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "product_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_product_options_on_product_id"
   end
 
   create_table "product_taxes", force: :cascade do |t|
     t.bigint "product_id", null: false
     t.bigint "tax_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_product_taxes_on_product_id"
     t.index ["tax_id"], name: "index_product_taxes_on_tax_id"
   end
@@ -84,7 +83,6 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
     t.bigint "product_id", null: false
     t.string "name"
     t.decimal "price", precision: 10, scale: 2, default: "0.0"
-    t.string "sku", null: false
     t.decimal "weight", precision: 8, scale: 2, default: "0.0"
     t.decimal "height", precision: 8, scale: 2, default: "0.0"
     t.decimal "width", precision: 8, scale: 2, default: "0.0"
@@ -92,23 +90,25 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
     t.boolean "is_main", default: false
     t.decimal "cost_price", precision: 10, scale: 2, default: "0.0"
     t.integer "position"
-    t.boolean "published", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "available_for_pick_up", default: true
+    t.boolean "available_for_expedition", default: true
+    t.string "sku", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_product_variants_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taxes", force: :cascade do |t|
     t.string "name"
     t.decimal "rate"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -118,10 +118,10 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
     t.string "last_name"
     t.string "admin_level", default: "seaman"
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -130,8 +130,8 @@ ActiveRecord::Schema.define(version: 2022_01_29_164130) do
     t.string "name"
     t.bigint "product_variant_id", null: false
     t.bigint "product_option_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["product_option_id"], name: "index_variant_values_on_product_option_id"
     t.index ["product_variant_id"], name: "index_variant_values_on_product_variant_id"
   end
